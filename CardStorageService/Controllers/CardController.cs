308b351fd6b1df1801bf22793a2e4cd84a71cf14
Oleg <serv_all@mail.ru>
2022-09-19 18:22:44 +0300

    Добавили пустые контроллеры, добавили конфиг логгера

---
+++ CardStorageService/Controllers/CardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardStorageService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        private readonly ILogger<CardController> logger;
+
+        public CardController(ILogger<CardController> Logger)
+        {
+            logger = Logger;
+        }
+
+        public IActionResult GetByClientId(string clientId)
+        {
+            return Ok();
+        }
+
+    }
+}
