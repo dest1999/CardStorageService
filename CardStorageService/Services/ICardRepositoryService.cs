dce2a7124738ff4b6d20fe3e811111140d70126d
Oleg <serv_all@mail.ru>
2022-09-21 16:55:00 +0300

    Добавлены интерфейсы репозиториев

---
+++ CardStorageService/Services/ICardRepositoryService.cs
@@ -0,0 +1,9 @@
+﻿using CardStorageService.Data;
+
+namespace CardStorageService.Services
+{
+    public interface ICardRepositoryService : IRepository<Card, int>
+    {
+        IList<Card> GetByClientId(int id);
+    }
+}
